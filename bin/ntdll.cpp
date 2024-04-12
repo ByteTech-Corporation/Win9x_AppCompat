c3bc8bffc544eace2ae925f564941ff99bdbc21e
Boonk3312 <theboonk7@hotmail.com>
2024-04-11 21:16:31 -0700

    Create ntdll.cpp

---
+++ bin/ntdll.cpp
@@ -0,0 +1,6 @@
+void ntdll() {
+     GetWindowsAppLibraryFunctionCalculation->0xD910E4;
+     GetWindowsAppLibraryProcess            ->['0E:1819FFC9A09', vxfat->0x00108D81 | memory<?__CallSystemLibEntry__>];
+     GetWindowsAppLibraryIcon               ->['B8:0E92019AF8F', vxfat->0x0DA912F3 | memory<?__CallDLLSystemUtil__>];
+     GetWindowsAppLibraryFile               ->['20:1918ADC9F82', vxfat->0xC82109D9 | memory<?__CallSystemAssembly__>];  
+}
